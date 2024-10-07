6d9eebe3c6510d122d92ebf6c47e1e43f914c570
Sophiya-ai <sophiyabr@gmail.com>
2024-10-07 13:12:50 +0200

    back to print only

--- main.py
+++ main.py
@@ -1,14 +1,2 @@
-<<<<<<< HEAD
-print("I am in conflict")
-=======
-a = 7 # создание переменной
-b = 9
-c = 10
 
-if a > b:
-    print("a>b")
-elif b>a:
-    print("a<b")
-else:
-    print("a=b")
->>>>>>> parent of a3caf34 (new code for and definition of a and b)
+print("I am in conflict")
